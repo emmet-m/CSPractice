cfc7dfef0088a6bd49bc06592e895d6f7d9b6628
Emmet Murray <e.minor.murray@gmail.com>
2018-06-26 22:17:47 +1000

    Added priority queue and test

---
+++ CSPracticeGraph/PriorityQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPracticeGraph
+{
+    /*
+     * Apparently, C# doesn't have priority queues in the stdlib...
+     * So here's my own!
+     */
+    public class PriorityQueue<T, E>
+        where E : IComparable
+    {
+        private List<Tuple<T, E>> elements;
+
+        public int Count {
+            get { return this.elements.Count; }
+        }
+
+        public PriorityQueue() {
+            this.elements = new List<Tuple<T,E>>(); // Start here, dynamically resize
+        }
+
+        public void Enqueue(T elem, E weight) {
+            int lo = 0, hi = this.elements.Count;
+
+            while (lo < hi) {
+                int mid = (lo + hi)/2;
+                if (this.elements.ElementAt(mid).Item2.CompareTo(weight) < 0)
+                {
+                    lo = mid + 1;
+                }
+                else if (this.elements.ElementAt(mid).Item2.CompareTo(weight) > 0)
+                {
+                    hi = mid;
+                }
+                else {
+                    lo = mid;
+                    hi = mid;
+                }
+            }
+
+            // This isn't really a good PriorityQueue because 
+            // insertion here is O(n) (I'm pretty sure), but it'll have to do for now :)
+            this.elements.Insert(lo, Tuple.Create(elem,weight));
+        }
+
+        public T Dequeue() {
+            if (this.elements.Count == 0)
+                throw new InvalidOperationException();
+
+
+            Tuple<T,E> toRet = this.elements.ElementAt(0);
+            this.elements.RemoveAt(0);
+            return toRet.Item1;
+        }
+    }
+}
---
+++ UnitTesting/PriorityQueueTesting.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CSPracticeGraph;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class PriorityQueueTesting
+    {
+        [TestMethod]
+        public void PriorityQueueTest()
+        {
+            PriorityQueue<String, Int32> pq = new PriorityQueue<String, Int32>();
+
+            Assert.AreEqual(0, pq.Count);
+            pq.Enqueue("Zero", 0);
+            Assert.AreEqual(1, pq.Count);
+            pq.Enqueue("One", 1);
+            Assert.AreEqual(2, pq.Count);
+            pq.Enqueue("NOne", -1);
+            Assert.AreEqual(3, pq.Count);
+
+            Assert.AreEqual("NOne", pq.Dequeue());
+            Assert.AreEqual("Zero", pq.Dequeue());
+            Assert.AreEqual("One", pq.Dequeue());
+        }
+
+        [TestMethod]
+        public void PriorityQueueStressTest()
+        {
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+            Random r = new Random();
+
+            for (int i = 0; i < 1000; i++) {
+                int rand = r.Next();
+                // Use weight as key
+                pq.Enqueue(rand, rand);
+            }
+
+            int prev = 0; // Dummy value to keep compiler happy
+            bool first = true;
+            while (pq.Count != 0) {
+                if (first) {
+                    prev = pq.Dequeue();
+                    first = false;
+                    continue;
+                }
+                int next = pq.Dequeue();
+                Assert.IsTrue(prev < next);
+                prev = next;
+            }
+        }
+    }
+}
